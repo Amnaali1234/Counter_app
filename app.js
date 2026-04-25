ed35874c598f51bd4b08ba312daa78a0d1a52680
Amnaali1234 <amnasheikh2255@gmail.com>
2026-04-25 23:55:03 +0500

    counter app

---
+++ app.js
@@ -0,0 +1,15 @@
+let count = 0;
+function increment() {
+    count ++;
+   document.getElementById('counter').innerHTML = count;
+}
+
+function decrement() {
+    count --;
+   document.getElementById('counter').innerHTML = count;
+}
+
+function reset() {
+    count = 0;
+    document.getElementById('counter').innerHTML = count;
+}
